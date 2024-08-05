15e4eb5157657e0b85b93462bdfa25882d5bf7eb
dilmurod1301 <dilmurod.1301@gmail.com>
2024-08-05 23:20:18 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+ismlar = ['Ali', 'Anvar', 'Daston', 'Bekzod', 'Abdulaziz', 'Kamron']
+for ism in ismlar:
+    print(f"{ism} do'stim qalaysan ishlaring yaxshimi")
+print("Kod 6 marta takrorlandi")
+
+sonlar = list(range(9,100,2))
+for son in sonlar:
+    print(f"{son} ning qubi {son**3} ga teng")
+
